baf52efcaf8b7b30248495c34bee36b37c8b95e9
JonAtliG <jonatli99@gmail.com>
2023-05-09 19:43:29 +0000

    laga model

--- menu/models.py
+++ menu/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
-    description = models:Charfield(max_length=999, blank=True)
+    description = models.CharField(max_length=999, blank=True)
     img = models.CharField(max_length=9999, blank=True)
     price = models.FloatField()
 
@@ -14,4 +14,4 @@ class Pizza(models.Model):
 class Topping(models.Model):
     name = models.CharField(max_length=255)
     img = models.CharField(max_length=9999, blank=True)
->>>>>>> f175445f3af19f9e72c98dc951752492d0cbc292
+
