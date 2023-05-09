d5bb6777f1e90bec01f74a64974e431141cae8c9
SiggiPallii <114510027+SiggiPallii@users.noreply.github.com>
2023-05-09 21:00:39 +0000

    some

--- menu/views.py
+++ menu/views.py
@@ -85,7 +85,7 @@ def sides_index(request):
 def create_pizza(request):
     if request.method == 'POST':
         form = create_pizza(data=request.POST)
-    else:
+        if form.is_valid():  pizza = form.save() else:
         form = PizzaCreateForm()
     return render(request, 'pizza/create_pizza.html', {
         'form': form
