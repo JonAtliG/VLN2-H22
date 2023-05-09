f175445f3af19f9e72c98dc951752492d0cbc292
TheodorBS <theodor22@ru.is>
2023-05-09 18:06:55 +0000

    crazy stuff

--- offers/models.py
+++ offers/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.CharField(max_length=9999, blank=True)
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.CharField(max_length=9999, blank=True)
+
+
+class Drinks(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.CharField(max_length=9999, blank=True)
+
+
+class Sides(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.CharField(max_length=9999, blank=True)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    img = models.CharField(max_length=9999, blank=True)
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    house_number = models.IntegerField()
+    country = models.CharField(max_length=255)
+    postal_code = models.IntegerField()
+    city = models.CharField(max_length=255)
+
+
+class CardInfo(models.Model):
+    name = models.CharField(max_length=255)
+    card_number = models.IntegerField()
+    exp_date = models.DateField()
+    cvc = models.IntegerField()
