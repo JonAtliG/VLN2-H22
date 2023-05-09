003d94b3f20f28e77894d17513f550763adca391
SiggiPallii <114510027+SiggiPallii@users.noreply.github.com>
2023-05-09 19:41:19 +0000

    some

--- menu/forms/pizza_custom.py
+++ menu/forms/pizza_custom.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm, widgets
+from django import forms
 from menu.models import Pizza
 class PizzaCreateCustom(ModelForm):
+    image = forms.CharField(required=True, widget=forms.TextInput(attrs={ 'class': 'form-control'}))
     class Meta:
        model = Pizza
        exclude = ['id']
        widgets = {
-           'name' : widgets.TextInput(attrs={'class': 'form-control'}),
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'description': widgets.TextInput(attrs={'class': 'form-control'}),
+            'category': widgets.Select(attrs={'class': 'form-control'}),
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'on_sale': widgets.ChekboxInput(attrs={'class': 'checkbox'})
        }
--- menu/urls.py
+++ menu/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.menu_index, name="menu-index"),
     path('pizzas', views.menu_index, name="menu-index"),
     path('drinks', views.drink_index, name="drink-index"),
-    path('sides', views.sides_index, name="side-index")
-]
+    path('sides', views.sides_index, name="side-index"),
+    path('create_candy', views.create_pizza, name="create_pizza")
+    ]
