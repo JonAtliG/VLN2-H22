c165c91a7b1f0f9eabc92452d6c7235967b47aaa
SiggiPallii <114510027+SiggiPallii@users.noreply.github.com>
2023-05-09 18:31:55 +0000

    something

--- menu/models.py
+++ menu/models.py
@@ -5,4 +5,6 @@ from django.db import models
 
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
+    description = models:Charfield(max_length=999, blank=True)
     img = models.CharField(max_length=9999, blank=True)
+    price = models.FloatField()
