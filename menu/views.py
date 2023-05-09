d53a27e5047cafad5a47427746885d188c75d62f
JonAtliG <jonatli99@gmail.com>
2023-05-09 19:28:35 +0000

    pizzur i menu

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import render
 
 # Create your views here.
+pizzas = [
+    {
+        'name': 'Seweroni Pizza',
+        'img': "/static/img/SeweroniPizza.png",
+        'desc': 'Pepperoni, Cheese, Pizza Sauce',
+        'price': 17.99,
+    },
+    {
+        'name': "Seweroni Pizza",
+        'img': "/static/img/SeweroniPizza.png",
+        'desc': "Pepperoni, Cheese, Pizza Sauce",
+        'price': 17.99
+    },
+    {
+        'name': "Hawaian Sewer",
+        'img': "/static/img/Hawaian%20Sewer.png",
+        'desc': "Ham, Pineapple, Bacon, Cheese, Pizza Sauce",
+        'price': 17.99
+    },
+    {
+        'name': "Spicy Sewage",
+        'img': "/static/img/SpicySewage.png",
+        'desc': "Jalapeno, Paprika, Mushrooms, Onion, Cheese, Pizza Sauce",
+        'price': 18.99
+    },
+    {
+        'name': "Sewage Saucy",
+        'img': "/static/img/SewageSaucy.png",
+        'desc': "Cheese, Pizza sauce",
+        'price': 11.99
+    },
+    {
+        'name': "Cheesy Manhole",
+        'img': "/static/img/CheesyManhole.png",
+        'desc': "Yellow Cheese, Mozzarella, Pepper Cheese, Pizza Sauce",
+        'price': 18.99
+    },
+    {
+        'name': "Shroomy Sewage Magic Sewshrooms",
+        'img': "/static/img/ShroomySewage.png",
+        'desc': "Mushrooms, Chicken, Pepper Cheese, Yellow Cheese, Pizza sauce",
+        'price': 19.99
+    },
+]
 
 def menu_index(request):
-    return render(request, 'menu/pizza_menu.html')
+    return render(request, 'menu/pizza_menu.html', context={'pizzas': pizzas})
 
 def drink_index(request):
     return render(request, 'menu/drink_menu.html')
