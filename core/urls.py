6381b18105746eb2a12ac0265d7f19e7785a7378
TheodorBS <theodor22@ru.is>
2023-05-10 19:56:40 +0000

    now you can log in

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.home_index, name="home-index"),
@@ -9,9 +9,9 @@ urlpatterns = [
     path('offers', views.offers_index, name="offers-index"),
 
     path('account/', views.account_index, name="account-index"),
+    path('account/login', LoginView.as_view(template_name="account/login_screen.html"), name='login'),
     path('account/login', views.account_login_index, name="login-index"),
     path('account/create', views.account_create_index, name="create-index"),
-
     path('menu', views.menu_index, name="menu-index"),
     path('menu/pizzas', views.pizza_menu_index, name="pizza-menu-index"),
     path('menu/drinks', views.drink_index, name="drink-index"),
